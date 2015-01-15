bd51e8d167fa4ad7ceac5e6a83457fc5f8cc874f
Sascha Theves <sascha.theves@gmail.com>
2015-01-15 20:35:22 +0100

    deleted unused code

--- src/test/java/denon4j/Avr1912Test.java
+++ src/test/java/denon4j/Avr1912Test.java
@@ -1,11 +1,5 @@
 package denon4j;
 
-import java.io.BufferedReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import org.junit.Test;
 
 import de.theves.denon4j.Avr1912;
@@ -45,46 +39,4 @@ public class Avr1912Test {
 		test.test();
 		System.out.println("Starting demo... Done.");
 	}
-
-	class Listener implements Runnable {
-		private InputStream in;
-
-		Listener(InputStream s) {
-			in = s;
-		}
-
-		@Override
-		public void run() {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(in));
-			try {
-				loop(reader);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		private void loop(BufferedReader reader) throws IOException,
-				InterruptedException {
-			while (!reader.ready()) {
-				Thread.sleep(100);
-			}
-
-			char[] buffer = new char[8];
-			int n = 0;
-			CharArrayWriter writer = new CharArrayWriter();
-			if (-1 != (n = reader.read(buffer))) {
-				writer.write(buffer, 0, n);
-			}
-			writer.flush();
-			System.out.println(new String(writer.toCharArray()));
-			loop(reader);
-		}
-
-	}
-
 }
