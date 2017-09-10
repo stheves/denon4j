fdee0964ffefae9836baab841482db531e4e2e01
Sascha Theves <sascha.theves@gmail.com>
2017-09-10 10:15:48 +0200

    #12 debug signing script

--- releng/prepare_signing.sh
+++ releng/prepare_signing.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_7033b92483e5_key -iv $encrypted_7033b92483e5_iv -in releng/codesigning.asc.enc -out releng/codesigning.asc -d
+echo "PWD: $(pwd)"
+echo "LS: $(ls -al)"
 gpg --fast-import releng/codesigningkey.asc
